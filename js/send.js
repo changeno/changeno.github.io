89f7934ee10c96b8cb76e82544b8294bbd56393b
changeno <58186809+changeno@users.noreply.github.com>
2020-03-07 14:14:04 +0300

    dont worry
    ea

--- js/send.js
+++ js/send.js
@@ -1,3 +1,18 @@
-function btnClickOk() {
-    alert('ok');
+var points = 0;
+var gain = 1;
+
+function addPoints() {
+    points += gain;
+    document.getElementById("point").textContent = "points: " + points
+}
+
+function gainInc() {
+    if (points > 9) {
+        points -= 10;
+        gain += 1
+        document.getElementById("point").textContent = "points: " + points
+    } else {
+        alert("not enough points");
+    }
+
 }
